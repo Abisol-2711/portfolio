af60862e40977db2e47d74c4b502db7fda17a72c
Sol <solabigail00@hotmail.com>
2023-11-02 16:18:11 -0300

    optimizacionDeJS

--- index.js
+++ index.js
@@ -3,34 +3,29 @@ const sobreMiSection = d.getElementById("sobreMi");
 const inicioSection = d.getElementById("inicio");
 const btnDownScroll = d.querySelector(".btn__down");
 const btnUpScroll = d.querySelector(".btn__up");
-
-
+const darkModeSwitch = d.getElementById("darkModeSwitch");
+const header = d.querySelector(".header");
+const aboutDark = d.querySelector(".about");
+const contactDark = d.querySelector(".contact");
+const footerDark = d.querySelector(".footer");
 
 btnDownScroll.addEventListener("click", () => {
-    sobreMi.scrollIntoView({ behavior: "smooth" });
+    sobreMiSection.scrollIntoView({ behavior: "smooth" });
 });
 
 btnUpScroll.addEventListener("click", () => {
     inicioSection.scrollIntoView({ behavior: "smooth" });
 });
 
-
-/*Dark Mode */
-const darkModeSwitch = d.getElementById("darkModeSwitch");
-const aboutDark = d.querySelector(".about");
-const contactDark = d.querySelector(".contact");
-const footerDark = d.querySelector(".footer");
-
 darkModeSwitch.addEventListener("click", () => {
-  if (!darkModeSwitch.checked) {
-    d.body.style.backgroundColor = "#b8abaf"; 
-    aboutDark.style.backgroundImage = "url('img/backgroundDark.png')";
-    contactDark.style.backgroundImage = "url('img/backgroundFooterDark.png')";
-    footerDark.style.backgroundImage = "url('img/backgroundFooterDark.png')";
-  } else {
-    d.body.style.backgroundColor = "#f4e7eb";
-    aboutDark.style.backgroundImage = "url('img/background.png')";
-    contactDark.style.backgroundImage = "url('img/backgroundFooter.png')";
-    footerDark.style.backgroundImage = "url('img/backgroundFooter.png')";
-  }
+    const isDarkMode = !darkModeSwitch.checked;
+    d.body.style.backgroundColor = isDarkMode ? "#5c3a44" : "#f4e7eb";
+    const imageSuffix = isDarkMode ? "Dark" : "";
+    aboutDark.style.backgroundImage = `url('img/background${imageSuffix}.png')`;
+    contactDark.style.backgroundImage = `url('img/backgroundFooter${imageSuffix}.png')`;
+    footerDark.style.backgroundImage = `url('img/backgroundFooter${imageSuffix}.png')`;
+});
+
+window.addEventListener("scroll", () => {
+    header.style.backgroundColor = window.scrollY > 700 ? "#6c5b7b" : "#6c5b7b24";
 });
